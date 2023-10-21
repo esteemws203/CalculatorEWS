431fb14268279ba3b5ca36831101e49bc18d7cbc
esteemws203 <esteemws203@gmail.com>
2023-10-22 01:46:15 +0700

    first commit code calculator

---
+++ CalculatorProgram/script.js
@@ -0,0 +1,77 @@
+class Calculator {
+    constructor() {
+        this.runningTotal = null;
+        this.buffer = '0';
+        this.previousOperator = null;
+        this.screen = document.querySelector('.screen');
+        this.init();
+    }
+
+    init() {
+        document.querySelector('.calc-buttons').addEventListener('click', (event) => {
+            this.buttonClick(event.target.innerText);
+        });
+    }
+
+    buttonClick(value) {
+        isNaN(value) ? this.handleSymbol(value) : this.handleNumber(value);
+        this.screen.innerText = this.buffer;
+    }
+
+    handleSymbol(symbol) {
+        switch (symbol) {
+            case 'C':
+                this.buffer = '0';
+                this.runningTotal = null;
+                break;
+            case '=':
+                if (this.previousOperator === null) return;
+                this.performCalculation(parseInt(this.buffer));
+                this.previousOperator = null;
+                this.buffer = '' + this.runningTotal;
+                this.runningTotal = null;
+                break;
+            case '←':
+                this.buffer = this.buffer.length === 1 ? '0' : this.buffer.slice(0, -1);
+                break;
+            default:
+                this.handleMath(symbol);
+                break;
+        }
+    }
+
+    handleMath(operator) {
+        if (this.buffer === '0') return;
+        const bufferValue = parseInt(this.buffer);
+        if (this.runningTotal === null) {
+            this.runningTotal = bufferValue;
+        } else {
+            this.performCalculation(bufferValue);
+        }
+        this.previousOperator = operator;
+        this.buffer = '0';
+    }
+
+    performCalculation(bufferValue) {
+        switch (this.previousOperator) {
+            case '+':
+                this.runningTotal += bufferValue;
+                break;
+            case '−':
+                this.runningTotal -= bufferValue;
+                break;
+            case '×':
+                this.runningTotal *= bufferValue;
+                break;
+            case '÷':
+                this.runningTotal /= bufferValue;
+                break;
+        }
+    }
+
+    handleNumber(numberString) {
+        this.buffer = this.buffer === '0' ? numberString : this.buffer + numberString;
+    }
+}
+
+new Calculator();
